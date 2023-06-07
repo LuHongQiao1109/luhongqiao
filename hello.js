d59df5ca740a440006f5c6aa7cf533d773e3e95a
lhongqiao <lhongqiao1109@163.com>
2023-06-07 15:14:43 +0800

    reset commit

---
+++ hello.js
@@ -0,0 +1,3 @@
+console.log('%c 🐘嘿嘿30🐘:', 'color: Indigo; background: Orchid; font-size: 20px;',11 )
+console.log('%c 🐘嘿嘿30🐘:', 'color: Indigo; background: Orchid; font-size: 20px;',22 )
+console.log('%c 🐘嘿嘿30🐘:', 'color: Indigo; background: Orchid; font-size: 20px;',33 )
